ba6ad3ff140238e5ae4c6a000108070bd64aae74
lucaArchidiacono <mail@lucaa.dev>
2024-12-28 11:46:02 +0100

    Initial commit

---
+++ EraGuess/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EraGuess
+//
+//  Created by DG-SM-8669 on 28.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EraGuess/EraGuessApp.swift
@@ -0,0 +1,17 @@
+//
+//  EraGuessApp.swift
+//  EraGuess
+//
+//  Created by DG-SM-8669 on 28.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct EraGuessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
